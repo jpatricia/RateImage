b8d7f1e199010f291a5e6639a029da5fb0fb480e
jpatricia <janice_patricia@live.com>
2016-03-11 22:42:42 -0500

    grid and list working ish
    still need to edit more stuff tho for layouts
    boxes are created for each images with its info(image and name only so
    far)
    havent added JScrollPane
    image resized if it's >1000 ->should limit it to >200 or 300 probably
    NOTE: apparently icon of toolbar must be resizable, so need to deal with
    this later

--- ImageCollectionModel.java
+++ ImageCollectionModel.java
@@ -5,16 +5,36 @@ public class ImageCollectionModel extends Observable{
     private boolean first = true;
     private Stars starButton;
     public ArrayList<ImageModel> ImageList;
+    public String viewMode;
+    public String change;
 
     ImageCollectionModel(){
         ImageList = new ArrayList<ImageModel>();
+        viewMode = "grid"; //default
+    }
+
+    public void getPrevImages(ArrayList<ImageModel> prev){
+        System.out.println("prev size: "+prev.size());
     }
 
     public void addImage(ImageModel m){
         ImageList.add(m);
         System.out.println(ImageList.size());
+        change = "add";
         setChanged();
         notifyObservers();
     }
+
+    public void setMode(String mode){
+        viewMode = mode;
+        change = "view";
+        setChanged();
+        notifyObservers();
+    }
+
+    public String getviewMode(){
+        return viewMode;
+    }
+
 
 }
--- ImageCollectionView.java
+++ ImageCollectionView.java
@@ -8,15 +8,16 @@ import java.util.Observer;
 
 public class ImageCollectionView extends JPanel implements Observer{
     private ImageCollectionModel model;
-   // private JScrollPane panel;
+    private JScrollPane panel;
 
     ImageCollectionView(ImageCollectionModel model_){
         model = model_;
+        panel = new JScrollPane();
 
-       // panel = new JScrollPane();
+        //this.setMaximumSize();
+        //panel.setBackground(new Color(192,233,240));
 
-        this.setLayout(new GridLayout());
-      //  panel.setBackground(new Color(192,233,240));
+        this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS)); //default
 
         ArrayList<ImageModel> list = model.ImageList;
         if(list.size() !=0){
@@ -34,53 +35,71 @@ public class ImageCollectionView extends JPanel implements Observer{
             }
         }
 
-       // this.add(panel);
     }
 
     public void loadImages(){
+
         ArrayList<ImageModel> list = model.ImageList;
-        if(list.size() !=0){
-            for(int i=0;i<list.size();i++){
-                JLabel image = new JLabel(list.get(i).getIcon());
-                JLabel name = new JLabel(list.get(i).getName());
-                String title = list.get(i).getName();
+        int size = list.size();
+        if(size !=0){
+            Box b = Box.createVerticalBox();
+            JLabel image = new JLabel();
+            JLabel name = new JLabel(list.get(size-1).getName());
+           // JLabel date = new JLabel(list.get(size-1).getDate());
+            String title = list.get(size-1).getName();
+            ImageIcon icon = list.get(size-1).getIcon();
 
-                image.setOpaque(true);
-                name.setOpaque(true);
+            int widthImg = icon.getIconWidth();
+            int heightImg = icon.getIconHeight();
 
-                image.addMouseListener(new MouseListener() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        JFrame imageWindow = new JFrame(title);
-                        imageWindow.add(image);
-                        imageWindow.setSize(image.getSize());
-                        imageWindow.setVisible(true);
-                        imageWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    }
+            System.out.println(widthImg+ " "+heightImg);
 
-                    @Override
-                    public void mousePressed(MouseEvent e) {}
-
-                    @Override
-                    public void mouseReleased(MouseEvent e) {}
-
-                    @Override
-                    public void mouseEntered(MouseEvent e) {}
-
-                    @Override
-                    public void mouseExited(MouseEvent e) {}
-                });
-
-                this.add(image);
-                this.add(name);
+            if(widthImg > 1000 || heightImg > 1000){
+                Image im = icon.getImage().getScaledInstance(widthImg/10,heightImg/10,Image.SCALE_SMOOTH);
+                icon =new ImageIcon(im);
             }
+
+            image.setIcon(icon);
+
+
+           // image.setOpaque(true); //these two are commented because
+           // name.setOpaque(true); //apparently it'll make bgrd grey
+
+            image.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    JFrame imageWindow = new JFrame(title);
+                    imageWindow.add(image);
+                    imageWindow.setSize(image.getSize());
+                    imageWindow.setVisible(true);
+                    imageWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                }
+
+                public void mousePressed(MouseEvent e) {}
+                public void mouseReleased(MouseEvent e) {}
+                public void mouseEntered(MouseEvent e) {}
+                public void mouseExited(MouseEvent e) {}
+            });
+
+            b.add(image);
+            b.add(name);
+            this.add(Box.createRigidArea(new Dimension(30,0)));
+            this.add(b);
         }
     }
 
     @Override
     public void update(Observable arg0, Object arg1) {
         System.out.println("update ImageCollection");
-        loadImages();
+        if(model.change == "add"){
+            loadImages();
+        }else if(model.change == "view"){
+            if(model.getviewMode() == "grid"){
+                this.setLayout(new FlowLayout());
+            }else if(model.getviewMode() == "list"){
+                this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+            }
+        }
         revalidate();
     }
 }
